f3be2ce528c6e8558f1c1ca0dc96ac934794d3f8
Ahmed Rashrash <ahmed.rashrash@gmail.com>
2026-07-13 10:06:14 -0400

    empty entity macro and basic components for testing it

---
+++ engine/src/core/components.rs
@@ -0,0 +1,13 @@
+use crate::Vector3;
+
+pub struct Position {
+    pub p: Vector3,
+}
+
+pub struct Velocity {
+    pub v: Vector3,
+}
+
+pub struct Acceleration {
+    pub a: Vector3,
+}
---
+++ engine/src/core/macros.rs
@@ -0,0 +1,10 @@
+macro_rules! entity {
+    () => {};
+}
+
+#[cfg(test)]
+mod tests {
+    pub fn it_works() {
+        entity!();
+    }
+}
