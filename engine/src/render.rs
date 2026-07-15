13266a2d70408fd2b218b6928f15c6fd311eb1f0
Ahmed Rashrash <ahmed.rashrash@gmail.com>
2026-07-15 11:32:07 -0400

    created RenderTarget type

--- engine/src/render.rs
+++ engine/src/render.rs
@@ -1,17 +1,27 @@
 pub mod vec_buf;
 
-use wgpu::{
-    Adapter, Device, DeviceDescriptor, ExperimentalFeatures, Features, Instance,
-    InstanceDescriptor, Limits, MemoryHints, PowerPreference, Queue, RequestAdapterOptions, Trace,
-};
+use std::sync::Arc;
 
-use crate::Float;
+use bytemuck::{Pod, Zeroable};
+use wgpu::{
+    Adapter, BufferUsages, Device, DeviceDescriptor, ExperimentalFeatures, Features, Instance,
+    InstanceDescriptor, Limits, MemoryHints, PowerPreference, Queue, RequestAdapterOptions,
+    Surface, Trace, VertexAttribute, VertexBufferLayout, VertexFormat, VertexStepMode,
+};
+use winit::window::Window;
+
+use crate::{Float, VecBuf, WgpuIndex};
 
 pub struct Renderer {
     instance: Instance,
-    device: Device,
-    queue: Queue,
+    device: Arc<Device>,
+    queue: Arc<Queue>,
     adapter: Adapter,
+
+    target: Option<RenderTarget>,
+
+    vertex: VecBuf<Vertex>,
+    index: VecBuf<WgpuIndex>,
 }
 
 impl Renderer {
@@ -35,19 +45,51 @@ impl Renderer {
                 trace: Trace::Off,
             })
             .await?;
+        let device = Arc::new(device);
+        let queue = Arc::new(queue);
+
+        let vertex = VecBuf::with_capacity(&device, &queue, 1024, BufferUsages::VERTEX);
+        let index = VecBuf::with_capacity(&device, &queue, 1024, BufferUsages::INDEX);
+
+        let vertex_layout = VertexBufferLayout {
+            array_stride: size_of::<Vertex>() as u64,
+            step_mode: VertexStepMode::Vertex,
+            attributes: &[
+                VertexAttribute {
+                    format: VertexFormat::Float32x3,
+                    offset: 0,
+                    shader_location: 0,
+                },
+                VertexAttribute {
+                    format: VertexFormat::Float32x3,
+                    offset: size_of::<[f32; 3]>() as u64,
+                    shader_location: 1,
+                },
+            ],
+        };
 
         Ok(Self {
             instance,
             device,
             queue,
+            vertex,
+            index,
             adapter,
         })
     }
+
+    pub fn render(&self) {}
 }
 
 #[repr(C)]
+#[derive(Pod, Zeroable, Clone, Copy)]
 pub struct Vertex {
     pub position: [Float; 3],
-    pub uv: [Float; 2],
-    pub normal: [Float; 3],
+    pub color: [Float; 3],
+    pub _padding: [Float; 2],
+}
+
+struct RenderTarget {
+    window: Arc<Window>,
+    surface: Surface<'static>,
 }
